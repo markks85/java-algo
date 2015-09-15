0482efd99830d6d16cc47fb35956f945ccbf0d62
HB <HB@HB-PC>
2015-09-15 17:02:11 +0900

    2015.09.15

--- java-algo/src/hello/Hello.java
+++ java-algo/src/hello/Hello.java
@@ -1,5 +1,14 @@
 package hello;
 
-public class Hello {
+import java.util.Scanner;
 
+public class Hello {
+	public static void main(String[] args) {
+	String	hello = "검색어를 입력해주세요"; 
+	System.out.println(hello);
+	Scanner scanner = new Scanner(System.in);
+	String hi = scanner.next();
+	System.out.println(hi);
+	
+	}
 }
