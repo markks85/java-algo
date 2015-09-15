f0e6c9cfef2641b248ada42889c4d4be487947ee
HB <HB@HB-PC>
2015-09-15 17:14:51 +0900

    a

--- java-algo/src/hello/Name6.java
+++ java-algo/src/hello/Name6.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Name6 {
 	public static void main(String[] args) {
-		String hello = "안녕하세요";
+		String hello = "안녕하세요..";
 		System.out.println(hello);
 		Scanner scanner = new Scanner (System.in);
 		String hi = scanner.next();
